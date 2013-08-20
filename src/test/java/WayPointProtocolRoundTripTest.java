36b9d434929ade4aa15f520f98ee9221d15c7713
ligi <ligi@ligi.de>
2013-08-21 00:46:50 +0200

    improve the test a bit

--- src/test/java/WayPointProtocolRoundTripTest.java
+++ src/test/java/WayPointProtocolRoundTripTest.java
@@ -2,20 +2,35 @@ import org.junit.Test;
 import org.ligi.androidhelper.test.TestSeeds;
 import org.ligi.ufo.MKHelper;
 import org.ligi.ufo.WayPoint;
+
+import static org.fest.assertions.Fail.fail;
 import static org.junit.Assert.assertTrue;
 
 public class WayPointProtocolRoundTripTest {
 
     @Test
-    public void WayPointsShouldBeTheSameAfterRoundTrip() {
+    public void WayPointWIthNegativeHeadingShouldBeTheSameAfterRoundTrip() {
         WayPoint pnt= TestSeeds.WAYPOINT3();
-        for (int i=-100;i<100;i++) {
-            pnt.setHeading(i);
-            WayPoint trippedPnt=new WayPoint(MKHelper.byte2intarr(pnt.toByteArray()),0);
-            assertTrue("Waypoints are not equal", trippedPnt.equals(pnt));
-
-        }
-
+        pnt.setHeading(-100);
+        WayPoint trippedPnt=new WayPoint(MKHelper.byte2intarr(pnt.toByteArray()),0);
+        assertTrue("Waypoints are not equal", trippedPnt.equals(pnt));
     }
+
+    @Test
+    public void WayPointWIthPositiveHeadingShouldBeTheSameAfterRoundTrip() {
+        WayPoint pnt= TestSeeds.WAYPOINT3();
+        pnt.setHeading(-0);
+        WayPoint trippedPnt=new WayPoint(MKHelper.byte2intarr(pnt.toByteArray()),0);
+        assertTrue("Waypoints are not equal", trippedPnt.equals(pnt));
+    }
+
+    @Test
+    public void WayPointWIthZeroHeadingShouldBeTheSameAfterRoundTrip() {
+        WayPoint pnt= TestSeeds.WAYPOINT3();
+        pnt.setHeading(0);
+        WayPoint trippedPnt=new WayPoint(MKHelper.byte2intarr(pnt.toByteArray()),0);
+        assertTrue("Waypoints are not equal", trippedPnt.equals(pnt));
+    }
+
 
 }
