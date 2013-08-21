99681a9bca07ea2ebe928fd45a02fe9b997d2c9d
ligi <ligi@ligi.de>
2013-08-21 21:31:36 +0200

    code style & simplify

--- src/java/org/ligi/ufo/DUBwiseHelper.java
+++ src/java/org/ligi/ufo/DUBwiseHelper.java
@@ -2,20 +2,16 @@ package org.ligi.ufo;
 
 public class DUBwiseHelper {
 
-
     public static String make2charsStr(int val) {
-        if (val < 10)
+        if (val < 10) {
             return "0" + val;
-        else
+        } else {
             return "" + val;
+        }
     }
 
     public static String seconds2str(int seconds) {
-        String res = "" + make2charsStr(seconds / 60) + ":" + make2charsStr(seconds % 60);
-        if (res.startsWith("0"))
-            return res.substring(1);
-        else
-            return res;
+        return String.valueOf(seconds / 60) + ":" + make2charsStr(seconds % 60);
     }
 
 
